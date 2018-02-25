9bf85d6d92e97c835e86f3ebce8047f5df734665
AymanGhaith <ayman.ghaith88@gmail.com>
2018-02-25 09:31:49 +0200

    solving 2 questions the first one will return the position of an alphabit and the second will return a string repeated ubon the passed number

--- strings.js
+++ strings.js
@@ -3,6 +3,12 @@
 // 	Ouput :: "Position of alphabet: 1"
 	function characPosit(character){
 		//your code is here
+		if (character.length !== 1){
+			return "Please enter one character"
+		}
+		var alphabet="abcdefghijklmnopqrstuvwxyz"
+
+		return alphabet.indexOf(character.toLowerCase())+1
 	}
 
 // Write a function called repeatStr which repeats the given
@@ -11,4 +17,8 @@
 	
 	function repeatStr (n, s) {
 		//your code is here
+		if (s === 0 ) {
+			return "";
+		}
+		return n + repeatStr(n, s-1);
 	}
